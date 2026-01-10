d175d68545c5896deebbc04a060fa3134ea7868a
MarioBarisa <130148782+MarioBarisa@users.noreply.github.com>
2026-01-10 14:15:02 +0000

    fixana profilna slika na telefonima

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,7 +12,7 @@
     </div>
     <div class="dropdown dropdown-end ml-auto">
       <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-        <div class="w-10 rounded-full">
+        <div class="w-10 sm:w-12 rounded-full">
           <img alt="Tailwind CSS Navbar component"
             src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
         </div>
